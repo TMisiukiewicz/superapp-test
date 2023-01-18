55f6e87a28be4060b326ca1633bf2dba49a262c6
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2023-01-18 09:59:01 +0100

    initial commit

---
+++ shared/dependencies.mjs
@@ -0,0 +1,12 @@
+const deps = {
+    react: {
+        singleton: true,
+        eager: true,
+        requiredVersion: '18.2.0',
+      },
+      'react-native': {
+        singleton: true,
+        eager: true,
+        requiredVersion: '0.70.5',
+      },
+}
